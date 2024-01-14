8fa9b9d26e50a08d76fb99f4772b3e69d12d391f
Desislav Damyanov <desislav.s.damyanov@gmail.com>
2024-01-14 04:10:36 +0200

    add prompt for version update

--- app.vue
+++ app.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+import { registerSW } from 'virtual:pwa-register'
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (!confirm('New version available. Refresh?')) return
+    updateSW()
+  },
+  onOfflineReady() {
+    console.log('App is offline-ready now.')
+  },
+})
 import Toaster from '@/components/ui/toast/Toaster.vue'
 </script>
 <template>
